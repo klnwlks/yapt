3108d9e0027659be25203622a42fbdda7fadf744
klnwlks <95198127+klnwlks@users.noreply.github.com>
2022-10-23 01:10:48 +0800

    pomo

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+// replace 0s with proper values
+
 #include <ncurses.h>
 #include <stdlib.h> 
 #include <unistd.h>
@@ -11,9 +13,24 @@ const char* header[] = {}; // move to a separate config.h
 const char* work[] = {}; // move to a separate config.h
 const char* rest[] = {}; // move to a separate config.h
 
+int W = 30, R = 5, C = 4;
+
 void resizeHandler(int sig){
     // do nothing lol
 }
+
+// after a (very painful) hour of attempting to figure out how to grab the size of a c string array,
+// i give up and resort to using a simpler solution
+void printHeader(WINDOW* win, const char** h, size_t s){
+    int y = 1, x = LINES - strlen(h[0]) / 2;
+
+    for (int i = 0; i < s; i++){
+	mvwaddstr(win, y, x, h[i]);
+	y++;
+    }
+    wrefresh(win);
+}
+
 
 void run(char* com, char** coml) {
     if (fork()) return;
@@ -21,28 +38,59 @@ void run(char* com, char** coml) {
     execvp(com, coml);
 }
 
+void recordStats(int m){
+    int w;
+}
+
 void alert() {
     run("notify-send", (char*[]){ "notify-send", "-u", "critical", "\"Pomodoro Alert\"", NULL });
     run("mpg123", (char*[]){ "mpg123", "-q", "alarm.mp3", NULL });
 }
 
-void timer(WINDOW* win, int c, status s){
+// setup actual timer itself, will also print
+// current status, header and time left.
+void timer(WINDOW* win, int t, int c, status s){
+    while (c >= 0) {
+	// wclear includes the box so i have to call this every clear
+	box(win, 0, 0);
+
+	mvwprintw(win, 0, 0, "Cycle %d of %d", c, C);
+	switch (s) {
+	    case WORK:
+		printHeader(win, work, 0);
+		mvwprintw(win, 0, 0, "Work.");
+		break;
+	    case REST:
+		printHeader(win, rest, 0);
+		mvwprintw(win, 0, 0, "Rest.");
+		break;
+	}
+
+	mvwprintw(win, 
+	    // set at middle
+	    0, 0,
+	    "%d:%d",
+	    t / 60, (t % 3600) % 60);
+
+
+	sleep(1);
+	wclear(win);
+	c--;
+    }
 }
 
+// setup window and loop for the cycles
 void startPomo(WINDOW* win){
-    int w, r, c;
+    WINDOW* pomo_win = newwin(0, 0, 0, 0);
     alert();
-    for (int i; i < c; i++){
+    for (int i; i < C; i++){
 	alert();
-	timer(win, c, WORK);
+	timer(win, W, i, WORK);
 	alert();
-	timer(win, c, WORK);
+	timer(win, C, i, WORK);
     }
     alert();
-}
-
-void recordStats(int m){
-    int w;
+    delwin(pomo_win);
 }
 
 void printStatus(WINDOW* win) {
@@ -51,16 +99,6 @@ void printStatus(WINDOW* win) {
 	    (COLS - 20) / 2);
     box(status, 0, 0);
     wrefresh(status);
-}
-
-void printHeader(WINDOW* win){
-    int y = 1, x = LINES - strlen(header[0]) / 2;
-
-    for (int i = 0; i < sizeof(header)/sizeof(header[0]); i++){
-	mvwaddstr(win, 0, x, header[i]);
-	y++;
-    }
-    wrefresh(win);
 }
 
 void printMenu(WINDOW* win){
@@ -99,18 +137,31 @@ void grabInput(WINDOW* win){
 }
 
 void pomoDashboard(){
+    // handler for resize and etc.
     signal(SIGWINCH, resizeHandler);
     WINDOW* dashboard = newwin(0, 0, COLS, LINES);
     box(dashboard, 0, 0);
 
-    printHeader(dashboard);
+    printHeader(dashboard, header, 0);
     printMenu(dashboard);
     wrefresh(dashboard);
 
     grabInput(dashboard);
+    // once function above ends, cleanup begins
+    delwin(dashboard);
 }
 
-int main(){
+void help() {}
+
+int main(int argc, char* argv[]){
+    if (argc > 3 && argc < 5) {
+	W = atoi(argv[1]);
+	R = atoi(argv[2]);
+	C = atoi(argv[3]);
+    } else { 
+	help(); 
+    }
+
     setlocale(LC_ALL, "");
 
     initscr();
