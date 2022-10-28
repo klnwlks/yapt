dd000b4e3049421c16ceaf3a5641f59dc81ea30e
klnwlks <95198127+klnwlks@users.noreply.github.com>
2022-10-28 19:18:24 +0800

    working interface

--- config.h
+++ config.h
@@ -1,9 +1,55 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-const char* header[] = {}; 
-const char* work[] = {}; 
-const char* menu[] = {}; 
-const char* rest[] = {};
+// strlen has trouble reading braille characters, so have an indice with all spaces 
+// matching the length of your header
+const char* header[] = { 
+"                              ",
+"⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣷⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ",
+"⠀⠀⠀⠀⠀⠀⠀⣀⣶⣿⣿⣿⣿⣿⣿⣷⣶⣶⣶⣦⣀⡀⠀⢀⣴⣇⠀⠀⠀ ",
+"⠀⠀⠀⠀⠀⢠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀ ",
+"⠀⠀⠀⠀⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀ ",
+"⠀⠀⠀⣴⣿⣿⣿⣿⠛⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⣿⣿⣿⣿⣿⣿⣿⣄⠀⠀ ",
+"⠀⠀⣾⣿⣿⣿⣿⣿⣶⣿⣯⣭⣬⣉⣽⣿⣿⣄⣼⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀ ",
+"⠀⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄",
+"⢸⣿⣿⣿⣿⠟⠋⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠁⣿⣿⣿⣿⡿⠛⠉⠉⠉⠉⠁",
+"⠘⠛⠛⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⠛⠛⠃⠀⠀⠀⠀  ⠀",
+"         pomodoro.            ",
+};
+const int hsize = 11;
+
+const char* menu[] = {
+" View Stats                j J",
+" Start Timer               k K",
+" Exit                      q Q"
+}; 
+const int msize = 3;
+
+const char* work[] = {
+"      )  (      ",
+"     (   ) )    ",
+"      ) ( (     ",
+"    _______)_   ",
+" .-'---------|  ",
+"( C|/\\/\\/\\/\\/|  ",
+" '-./\\/\\/\\/\\/|  ",
+"   '_________'  ",
+"    '-------'   "
+}; 
+const int wsize = 8;
+
+const char* rest[] = {
+"      )  (      ",
+"     (   ) )    ",
+"      ) ( (     ",
+"    _______)_   ",
+" .-'---------|  ",
+"( C|/\\/\\/\\/\\/|  ",
+" '-./\\/\\/\\/\\/|  ",
+"   '_________'  ",
+"    '-------'   "
+};
+const int rsize = 8;
+
 
 #endif
--- main.c
+++ main.c
@@ -1,5 +1,3 @@
-// replace 0s with proper values
-
 #include <ncurses.h>
 #include <stdlib.h> 
 #include <unistd.h>
@@ -7,6 +5,7 @@
 #include <locale.h>
 #include <string.h>
 #include <time.h>
+#include <math.h>
 
 #include "config.h"
 
@@ -20,10 +19,10 @@ void resizeHandler(int sig){
 // after a (very painful) hour of attempting to figure out how to grab the size of a c string array,
 // i give up and resort to using a simpler solution
 void printHeader(WINDOW* win, const char** h, size_t s){
-    int y = 1, x = LINES - strlen(h[0]) / 2;
+    int y = 1, x = (COLS - strlen(h[0])) / 2;
 
     for (int i = 0; i < s; i++){
-	mvwaddstr(win, y, x, h[i]);
+	mvwprintw(win, y, x, "%s", h[i]);
 	y++;
     }
     wrefresh(win);
@@ -37,10 +36,40 @@ void run(char* com, char** coml) {
 }
 
 void recordStats(int m){
-    int w;
+    // hours, sessions, then the same but weekly
+    float h = 0, wh = 0;
+    int s = 0, ws = 0; 
+    FILE* f = fopen("stats", "w+"); // open for read and write
+    if (!f) return; // replace with exception
+
+    char day[10];
+    time_t r_time = time(0);
+    struct tm* tinfo = localtime(&r_time);
+    strftime(day, 10, "%a", tinfo);
+
+    // read stats
+    fscanf(f, "%f %d %f %d", &h, &s, &wh, &ws); 
+
+    h += fmod((float)m, 60.0); 
+    s++;
+
+    // if a week has passed, refresh statistics
+    if (ws > 0 && !strcmp(day, "Sun")) {
+	wh = 0;
+	ws = 0;
+    } else {
+	wh += fmod((float)m, 60.0); 
+	ws++;
+    }
+
+
+    // write stats
+    fprintf(f, "%f %d %f %d", h, s, wh, ws);
+    fclose(f);
 }
 
 void alert() {
+    // dunno if this is the best solution
     run("notify-send", (char*[]){ "notify-send", "-u", "critical", "\"Pomodoro Alert\"", NULL });
     run("mpg123", (char*[]){ "mpg123", "-q", "alarm.mp3", NULL });
 }
@@ -56,11 +85,11 @@ void timer(WINDOW* win, int t, int c, status s){
 	switch (s) {
 	    case WORK:
 		printHeader(win, work, 0);
-		mvwprintw(win, 0, 0, "Work.");
+		mvwprintw(win, 0, 0, " work.");
 		break;
 	    case REST:
 		printHeader(win, rest, 0);
-		mvwprintw(win, 0, 0, "Rest.");
+		mvwprintw(win, 0, 0, " rest.");
 		break;
 	}
 
@@ -71,15 +100,41 @@ void timer(WINDOW* win, int t, int c, status s){
 	    t / 60, (t % 3600) % 60);
 
 
+	wrefresh(win);
 	sleep(1);
 	wclear(win);
 	c--;
     }
 }
 
+void printStats(WINDOW* win) {
+    float h = 0, wh = 0;
+    int s = 0, ws = 0; 
+    FILE* f = fopen("stats", "r"); // open for read
+    if (!f) return; // replace with exception
+
+    // read stats
+    fscanf(f, "%f %d %f %d", &h, &s, &wh, &ws); 
+
+    WINDOW* stats = newwin(0, 0, 
+	    (LINES - 0) / 2, 
+	    (COLS - 0) / 2);
+    box(stats, 0, 0);
+    mvwprintw(stats, 0, 0, "");
+    wrefresh(stats);
+
+    getch();
+    delwin(stats);
+    wrefresh(win);
+    fclose(f);
+}
+
+
 // setup window and loop for the cycles
 void startPomo(WINDOW* win){
-    WINDOW* pomo_win = newwin(0, 0, 0, 0);
+    WINDOW* pomo_win = newwin(0, 0,
+	    (LINES - 0) / 2, 
+	    (COLS - 0) / 2);
     alert();
     for (int i; i < C; i++){
 	alert();
@@ -89,22 +144,18 @@ void startPomo(WINDOW* win){
     }
     alert();
     delwin(pomo_win);
-}
+    wrefresh(win);
 
-void printStatus(WINDOW* win) {
-    WINDOW* status = newwin(8, 20, 
-	    (LINES - 8) / 2, 
-	    (COLS - 20) / 2);
-    box(status, 0, 0);
-    wrefresh(status);
+    recordStats(W * C);
+    printStats(win);
 }
 
 void printMenu(WINDOW* win){
-    int y = (sizeof(header)/sizeof(header[0]) + 3),
-	x = LINES - strlen(menu[0]) / 2;
+    int y = hsize + 3,
+	x = (COLS - strlen(menu[0])) / 2;
 
-    for (int i = 0; i < sizeof(menu)/sizeof(menu[0]); i++){
-	mvwaddstr(win, 0, x, menu[i]);
+    for (int i = 0; i < msize; i++){
+	mvwprintw(win, y, x, "%s", menu[i]);
 	y++;
     }
     wrefresh(win);
@@ -117,7 +168,7 @@ void grabInput(WINDOW* win){
 	switch(ch) {
 	    case 'j':
 	    case 'J':
-		printStatus(win);
+		printStats(win);
 		break;
 
 	    case 'k':
@@ -129,17 +180,19 @@ void grabInput(WINDOW* win){
 	    case 'Q':
 		return;
 		break;
+
+	    default:
+		continue;
 	}
     }
 }
 
 void pomoDashboard(){
     // handler for resize and etc.
-    signal(SIGWINCH, resizeHandler);
-    WINDOW* dashboard = newwin(0, 0, COLS, LINES);
+    WINDOW* dashboard = newwin(LINES, COLS, 0, 0);
     box(dashboard, 0, 0);
 
-    printHeader(dashboard, header, 0);
+    printHeader(dashboard, header, hsize);
     printMenu(dashboard);
     wrefresh(dashboard);
 
@@ -148,7 +201,9 @@ void pomoDashboard(){
     delwin(dashboard);
 }
 
-void help() {}
+void help() {
+    printf("usage: yapt [work] [rest] [cycles] \n");
+}
 
 int main(int argc, char* argv[]){
     if (argc > 3 && argc < 5) {
@@ -157,13 +212,12 @@ int main(int argc, char* argv[]){
 	C = atoi(argv[3]);
     } else { 
 	help(); 
+	return 1;
     }
 
     setlocale(LC_ALL, "");
 
     initscr();
-    noecho();
-    raw();
     pomoDashboard();
 
     endwin();
